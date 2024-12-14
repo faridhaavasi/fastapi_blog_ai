072305eba6ce9eaacbd583dd33329246e4a9b43d
faridhaavasi <faridhavasi777@gmail.com>
2024-12-14 18:43:18 +0330

    path and query paramiter in fast api

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'message': 'hi fastapi'}
+@app.get('/home/{name}')
+def index(name: str, age :int=0):
+    return {'message': f" hi  {name} {age} yers old"}
