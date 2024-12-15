7d49f97d033dd9423dc21127a212e012da73d10f
faridhaavasi <faridhavasi777@gmail.com>
2024-12-15 22:17:47 +0330

    request body

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Person(BaseModel):
+    name : str
+    age: int
+    hirgt: int | None = None
 
 app = FastAPI()
 
 @app.get('/home/{name}')
 def index(name: str, age :int=0):
     return {'message': f" hi  {name} {age} yers old"}
+
+@app.post('/post_page')
+def post_page(person_data: Person):
+    return person_data    
+    
