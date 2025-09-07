6da0642141bb043bc2d5ea821e11971887c756a8
faridhaavsi <faridhavasi777@gmail.com>
2025-09-07 17:25:15 +0330

    add detrail in app for main

--- service/main.py
+++ service/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI 
 
 
-app = FastAPI()
+app = FastAPI(
+     title="Blog Application",
+    description=(
+        "A simple and efficient blog advance API built with FastAPI. "
+        "This API allows users to create, retrieve, update, and delete post. "
+        "It is designed for task tracking and productivity improvement."
+    ),
+    version="1.0.0",
+)
 
-@app.get('/')
-async def index():
-    return {}
+
+
