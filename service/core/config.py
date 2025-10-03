4f94dd1e35ec3ac85bd4c7ceb3dc272f1d1e2a74
faridhaavsi <faridhavasi777@gmail.com>
2025-10-03 14:03:33 +0330

    resert and start new idea

---
+++ service/core/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    SQLALCHEMY_DATABASE_URL: str
+    JWT_SECRET_KEY : str = 'test'
+
+    class Config:
+        env_file = "../.env"
+
+settings = Settings()
