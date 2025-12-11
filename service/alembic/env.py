a9fe83abc5aa437d4fe1aebdbca56da850af41c9
mohammad mahdi takfallah <mmtmmt945@gmail.com>
2025-12-11 10:08:10 +0330

    feat: chatbot MessageModel is now added to the env.py file now.

--- service/alembic/env.py
+++ service/alembic/env.py
@@ -50,6 +50,7 @@ target_metadata = Base.metadata
 
 from user.api.v1.models import UserModel, TokenModel
 from post.api.v1.models import PostModel, LikeModel, CommentModel
+from chatbot.api.v1.models import MessageModel
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
